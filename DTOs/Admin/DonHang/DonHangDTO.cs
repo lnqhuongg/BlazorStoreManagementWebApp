143dd650a7e44b7c340fe90c9397342bd49105f3
Gillion249 <161831406+Gillion249@users.noreply.github.com>
2025-12-04 22:09:27 +0700

    giaodien

---
+++ DTOs/Admin/DonHang/DonHangDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorStoreManagementWebApp.DTOs.Admin.DonHang
+{
+    public class DonHangDTO
+    {
+        public int OrderId { get; set; }
+        public string CustomerName { get; set; } = "Khách vãng lai";
+        public DateTime? OrderDate { get; set; }
+        public decimal? TotalAmount { get; set; }
+        public string PaymentStatus { get; set; } = "Chưa thanh toán";
+    }
+}
---
+++ DTOs/Admin/NhaCungCap/NhaCungCapDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorStoreManagementWebApp.DTOs.Admin.NhaCungCap
+{
+    public class NhaCungCapDTO
+    {
+        public int SupplierId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+        public bool Status { get; set; } = true; // Mapping từ bit(1)
+    }
+}
