1e441ca0ef9669dbc70bb7331261e4dfd9b5b846
Congdeho <congdeho98@gmail.com>
2025-12-07 22:50:37 +0700

    fix magiamgia

---
+++ Services/Interfaces/IMaGiamGiaService.cs
@@ -0,0 +1,17 @@
+﻿using BlazorStoreManagementWebApp.DTOs.Admin.MaGiamGia;
+using BlazorStoreManagementWebApp.Helpers;
+
+namespace BlazorStoreManagementWebApp.Services.Interfaces
+{
+    public interface IMaGiamGiaService
+    {
+        Task<PagedResult<MaGiamGiaDTO>> GetAll(int page, int pageSize, string? keyword, string? discountType);
+        Task<MaGiamGiaDTO?> GetById(int id);
+        Task<MaGiamGiaDTO> Create(MaGiamGiaDTO dto);
+        Task<MaGiamGiaDTO?> Update(int id, MaGiamGiaDTO dto);
+        Task<bool> Delete(int id);
+        Task<List<MaGiamGiaDTO>> SearchByKeyword(string keyword);
+        Task<List<MaGiamGiaDTO>> GetAllActive();
+        Task<MaGiamGiaDTO?> updateAfterCreatedOrder(int? promoId);
+    }
+}
