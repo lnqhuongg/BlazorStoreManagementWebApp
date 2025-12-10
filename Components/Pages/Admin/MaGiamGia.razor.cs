7a0d52eb5d87b8e0494c10d180d588969166bef7
Congdeho <congdeho98@gmail.com>
2025-12-10 23:29:39 +0700

    merge master

---
+++ Components/Pages/Admin/MaGiamGia.razor.cs
@@ -0,0 +1,57 @@
+﻿using BlazorStoreManagementWebApp.Components.Forms.Admin;
+using BlazorStoreManagementWebApp.DTOs.Admin.MaGiamGia;
+using BlazorStoreManagementWebApp.Helpers;
+using BlazorStoreManagementWebApp.Services.Interfaces;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace BlazorStoreManagementWebApp.Components.Pages.Admin
+{
+    public partial class MaGiamGia : ComponentBase
+    {
+        [Inject] private IMaGiamGiaService MaGiamGiaService { get; set; } = default!;
+        private MaGiamGiaForm MaGiamGiaFormRef = default!;
+        private PagedResult<MaGiamGiaDTO>? PromoData;
+        private int Page = 1;
+        private int PageSize = 10;
+        private string Keyword = "";
+        private string DiscountType = "";
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadData();
+        }
+
+        private async Task LoadData()
+        {
+            PromoData = await MaGiamGiaService.GetAll(Page, PageSize, Keyword, DiscountType);
+            StateHasChanged();
+        }
+
+        private async Task Search()
+        {
+            Page = 1;
+            await LoadData();
+        }
+
+        private async Task ChangePage(int newPage)
+        {
+            Page = newPage;
+            await LoadData();
+        }
+        private async Task OnSearchKeyDown(KeyboardEventArgs e)
+        {
+            if (e.Key == "Enter")
+            {
+                await Search();
+            }
+        }
+        private async Task FilterChanged(ChangeEventArgs e)
+        {
+            Page = 1;
+            await LoadData();
+        }
+
+
+    }
+}
