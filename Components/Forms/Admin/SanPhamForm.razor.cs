a22f7be94eb6d5bf38b35f2d8732fd21a0a429ce
lnqhuongg <cuubong47@gmail.com>
2025-12-16 12:07:01 +0700

    little things fixed

--- Components/Forms/Admin/SanPhamForm.razor.cs
+++ Components/Forms/Admin/SanPhamForm.razor.cs
@@ -111,7 +111,6 @@ namespace BlazorStoreManagementWebApp.Components.Forms.Admin
             productDTO = dto;
             StateHasChanged(); // Đảm bảo UI cập nhật tiêu đề
             await JS.InvokeVoidAsync("showBootstrapModal", "addProductModal");
-            await JS.InvokeAsync<object>("showToast", "success", "Cập nhật sản phẩm thành công!");
         }
         public async Task OpenView(SanPhamDTO dto, List<LoaiSanPhamDTO> LSPData, List<NhaCungCapDTO> NCCData)
         {
